da442112f9515361634041f159741b7916b6adcc
DangNhuNam <115100585+DangNhuNam@users.noreply.github.com>
2022-12-07 01:51:54 +0700

    Create main.php

---
+++ main.php
@@ -0,0 +1,21 @@
+<?php
+class UserAgents {
+public static function User_Agent() {
+              $path = 'user-agents.txt';
+              $fp = @fopen($path, "r");
+             if (!$fp) {
+                die('Sai đường dẫn vui lòng kiểm tra lại');
+             } else {
+// Đọc từng dòng
+              while(!feof($fp))
+             {
+              $ua[] = fgets($fp);
+             }
+              fclose($fp);
+              $random = rand(0,count($ua)-1);
+              return $ua[$random];
+            }
+                fclose($file);
+     }
+ }
+ print_r(UserAgents::User_Agent());
